088c6a2600ecab216d2d5d7241159524ceae4e69
Pedro Lucas <plrf100@gmail.com>
2020-01-12 18:46:04 -0300

    without cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const morgan = require('morgan');
-const cors = require('cors');
+//const cors = require('cors');
 
 require('dotenv').config();
 

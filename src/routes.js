7d1d974200c75427862068998d12a5e736dd3daf
Pedro Lucas <plrf100@gmail.com>
2020-01-12 18:39:17 -0300

    removed cors from here

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const routes = express.Router();
 const InfoController = require('./controllers/InfoController');
-var cors = require('cors')
 
-routes.get('/', cors(), InfoController.index);
-routes.get('/last', cors(), InfoController.last_index);
-routes.get('/first', cors(), InfoController.first_index);
-routes.post('/send', cors(), InfoController.send);
-routes.delete('/delete/:id', cors(), InfoController.destroy);
+
+routes.get('/', InfoController.index);
+routes.get('/last', InfoController.last_index);
+routes.get('/first', InfoController.first_index);
+routes.post('/send', InfoController.send);
+routes.delete('/delete/:id', InfoController.destroy);
 
 module.exports = routes;
